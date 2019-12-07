e3b78333fd097230194c224a10d7b4e477ff9806
Jivansh Sharma <jivansh17@gmail.com>
2019-12-07 21:37:57 +0530

    Extract Y/N branches into Functions

--- todo.py
+++ todo.py
@@ -1,15 +1,24 @@
 todo_list = []
 
+def get_item_input():
+    item = input("Item > ")
+    todo_list.append(item)
+
+def say_goodbye_to_user():
+    print("Your todo list: ", todo_list)
+    print("Good luck completing it!")
+        
+
 while True:
     print("Your current list is", todo_list)
     answer = input("Do you want to add an item? (y/n): ")
     if answer == "Y" or answer == "y":
-        item = input("Item > ")
-        todo_list.append(item)
+        get_item_input()
     elif answer == "N" or answer == "n":
-        print("Your todo list: ", todo_list)
-        print("Good luck completing it!")
+        say_goodbye_to_user()
         break
     else:
         print("Invalid input")
+
+
     
