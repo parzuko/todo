f0ade1b12fa38427168eb0aa6477f307cebf0410
Jivansh Sharma <jivansh17@gmail.com>
2019-12-07 21:15:50 +0530

    Create basic todo application

--- todo.py
+++ todo.py
@@ -1,1 +1,14 @@
-print("New todo app")
+todo_list = []
+
+while True:
+    answer = input("Do you want to add an item? (y/n): ")
+    if answer == "N" or answer == "n":
+        print("Your todo list is: ", todo_list)
+        break
+    elif answer == "Y" or answer == "y":
+        item = input("Item > ")
+        todo_list.append(item)
+        print("Your todo list is: ", todo_list)
+    else:
+        print("Invalid input")
+    
