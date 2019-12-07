71544c3febde7ca51eefa2777f5e51ed71c45d18
Jivansh Sharma <jivansh17@gmail.com>
2019-12-07 21:55:05 +0530

    Create display function for todo list

--- todo.py
+++ todo.py
@@ -1,16 +1,28 @@
 todo_list = []
 
+def display_todo_list():
+    if len(todo_list) == 0:
+        print("No Items in List")
+    else:
+        for index, item in enumerate(todo_list):
+            print(index+1, item)
+    print("\n")
+        
+
 def get_item_input():
     item = input("Item > ")
     todo_list.append(item)
 
+
 def say_goodbye_to_user():
-    print("Your todo list: ", todo_list)
+    print("Your todo list: ")
+    display_todo_list()
     print("Good luck completing it!")
-        
+
 
 while True:
-    print("Your current list is", todo_list)
+    print("Your current list is: ")
+    display_todo_list()
     answer = input("Do you want to add an item? (y/n): ")
     if answer == "Y" or answer == "y":
         get_item_input()
@@ -19,6 +31,3 @@ while True:
         break
     else:
         print("Invalid input")
-
-
-    
